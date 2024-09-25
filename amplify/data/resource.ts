e24bfa69d66e20490f041bb0d03828951ac89200
duyentth <duyen.tran@miu.edu>
2024-09-25 15:56:34 -0400

    updated authorizationModes from iam to apikey

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -33,7 +33,10 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "iam",
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 365,
+    },
   },
 });
 
